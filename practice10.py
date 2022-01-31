58afa9084e2728423e433eefb355d203d0f8970f
doyle <doylehot@gmail.com>
2022-01-31 23:47:37 +0900

    7287성공!

--- practice10.py
+++ practice10.py
@@ -1,2 +1,23 @@
-#2525번문제
+
+A, B = map(int, input().split())
+C = int(input())
+
+C_B_hour = (C + B) // 60
+C_B_minute = (C + B) % 60
+
+A_day = (A + C_B_hour) // 24
+
+A_hour = (A + C_B_hour) % 24
+
+if A_day == 0:
+    print(f'{A + C_B_hour} {C_B_minute}')
+
+elif A_day > 0:
+    print(f'{A_hour} {C_B_minute}')
+    
+    
+    
+
+
+
 
