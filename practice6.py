5e9a31ab9a46b86647e8e8a9d7eaf4bbc5edbb1a
doyle <doylehot@gmail.com>
2022-01-31 18:48:07 +0900

    11021성공

---
+++ practice6.py
@@ -0,0 +1,37 @@
+# 11021번문재
+
+
+
+
+
+# A, B = map(int, input().split())
+# C, D = map(int, input().split())
+# E, F = map(int, input().split())
+# G, H = map(int, input().split())
+# I, J = map(int, input().split())
+
+# print(f'Case #1: {A + B}')
+# print(f'Case #2: {C + D}')
+# print(f'Case #3: {E + F}')
+# print(f'Case #4: {G + H}')
+# print(f'Case #5: {I + J}')
+
+
+
+
+# test_data = int(input())
+
+
+test_data = int(input())
+
+for num in range(1, test_data + 1):
+    A, B = map(int, input().split())
+    print(f'Case #{num}: {A + B}')
+    
+
+    
+        
+
+
+
+
