4c0932e9931805a91e06386bc7987db62afa8923
doyle <doylehot@gmail.com>
2022-02-01 16:58:57 +0900

    2935성공

---
+++ practice15.py
@@ -0,0 +1,13 @@
+# 2935번
+
+first_num = int(input())
+
+oper = input()
+
+second_num = int(input())
+
+if oper == '*':
+    print(first_num * second_num)
+
+elif oper == '+':
+    print(first_num + second_num)
