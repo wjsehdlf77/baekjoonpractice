0ff47e5d0edc6ad8fcfeb8f907057327978f61e8
Do Il Jeon <98448316+wjsehdlf77@users.noreply.github.com>
2022-02-06 19:16:23 +0900

    어렵당

--- practice26.py
+++ practice26.py
@@ -2,8 +2,39 @@
 
 from sys import stdin
 
-for a in range(3):
 
+
+beacon_x = []
+beacon_y = []
+for a in range(3):
+    
     beacon1, beacon2 = map(int, stdin.readline().split())
+
+    beacon_x.append(beacon1)
+    beacon_y.append(beacon2)
+
+for num in range(3):
+    if beacon_x.count(beacon_x[num]) == 1:
+        beacon1 = beacon_x[num]
+
+    if beacon_y.count(beacon_y[num]) == 1:
+        beacon2 = beacon_y[num]
+
+print(beacon1, beacon2)
+
+
+
+        
+
+
+            
+
+
+    
+
+
+
+
+
 
     
