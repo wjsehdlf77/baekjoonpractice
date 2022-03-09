334bccf87c03fe14356ad53c2d074d938e68715f
Do Il Jeon <98448316+wjsehdlf77@users.noreply.github.com>
2022-03-10 00:22:32 +0900

    정답

--- practice40.py
+++ practice40.py
@@ -2,17 +2,20 @@
 
 from sys import stdin
 
-
-
 while True:
-    a = []
+    list =[]
     data = int(stdin.readline())
     if data == -1:
         break
     for num in range(1, data):
         if data % num == 0:
             list.append(num)
-        if sum(list) == data:
-            print()
-    
+    if sum(list) == data:
+        asd = ' + '.join(map(str, list))
+        print(f"{data} = {' + '.join(map(str, list))}")
+            
+    else:
+        print(f"{data} is NOT perfect.")
+        list = []
+
 
