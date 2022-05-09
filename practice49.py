6b3ff3b2e88ab79c4e1041ae4cade49f1f86159f
Do Il Jeon <98448316+wjsehdlf77@users.noreply.github.com>
2022-05-09 23:27:21 +0900

    아몰랑

--- practice49.py
+++ practice49.py
@@ -11,18 +11,26 @@ for _ in range(t):
     data.append(int(stdin.readline()))
 
 dp = [0] * (max(data) + 1)
-dp[1] = 1
-dp[2] = 2
-dp[3] = 3
+# dp[1] = 1
+# dp[2] = 2
+# dp[3] = 3
 
-for i in range(4, max(data) + 1)
+# for i in range(4, max(data) + 1)
+
+def count(num):
+    if num == 0:
+        return 1
+    elif num < 0:
+        return 0
+    dp[num] = count(num - 1) + count(num - 2) + count(num - 3)
+
+print(count(4))
+    
 
 
 
 
-
-
-for i in data:
-    print([dp[i]])
+# for i in data:
+#     print([dp[i]])
 
 
